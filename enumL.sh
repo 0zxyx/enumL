1a8b5f8d6ac18bec6ecb7ce031b6d8433b50789e
0zxyx <67796396+0zxyx@users.noreply.github.com>
2023-04-01 02:12:15 -0400

    Update enumL.sh

--- enumL.sh
+++ enumL.sh
@@ -24,6 +24,7 @@ echo
 echo "Grabbing Who Is Logged On"
 echo 
 w
+pinky
 
 
 echo 
@@ -60,16 +61,12 @@ ps -ef
 
 
 echo 
-echo "Can $(whoami) Read Shadow"
+echo "Can $(whoami) Read/Write passwd/shadow Files"
 echo 
 cat /etc/shadow
-
-
-echo 
-echo "Can $(whoami) Write to Password"
-echo 
-getfacl /etc/passwd
-attr -l /etc/passwd
+ls -la /etc/{passwd,shadow}
+getfacl /etc/{passwd,shadow}
+attr -l /etc/{passwd,shadow}
 
 
 echo 
@@ -106,9 +103,15 @@ getcap -r / 2>/dev/null
 
 
 echo 
-echo "Grabbing Files Drop By Users Inside /usr/local/sbin/ /usr/sbin/ /usr/bin /sbin /bin"
+echo "Grabbing Files Drop By Users Inside /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin"
 echo 
-for i in /usr/local/sbin/ /usr/sbin/ /usr/bin /sbin /bin ; do find $i -type f  -printf "%T+ %p\n"  |grep -v 00000000;done
+for i in /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin ; do find $i -type f  -printf "%T+ %p\n"  |grep -v 00000000;done
+
+
+echo 
+echo "Can $(whoami) Write Inside /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin"
+echo 
+find /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin -writable  2>/dev/null
 
 
 echo 
@@ -156,7 +159,19 @@ find /var/www/ -type f |head -n 100
 echo 
 echo "Grabbing Passwords From /var/www, /home, /etc"
 echo 
-grep --exclude={*.css,*.js.map,*.css.map,*.min.map,*.svg} -ri 'pwd\|passwd\|password\|dbuser\|dbpass\|dbpwd\|PWD\|credential\|token' /var/www /home/ /etc 2>/dev/null
+grep --exclude={*.css,*.js.map,*.css.map,*.min.map,*.svg} -Ri 'pwd\|passwd\|password\|dbuser\|db_user\|user\|db_password\|db_passwd\|dbpass\|dbpwd\|PWD\|credential\|token\|postgresql' /var/www /home/ /etc 2>/dev/null
+
+
+echo 
+echo "Grabbing .htpasswd/.htaccess From /"
+echo
+find / \( -iname ".htpasswd" -o -iname ".htaccess" \)  2>/dev/null
+
+
+echo 
+echo "Grabbing php Dangerous Functions From /var/www/"
+echo
+grep -Ri 'system\|exec\|passthru\|shell_exec\|`\|popen\|proc_open\|pcntl_exec\|file_put_contents\|file_get_contents\|ob_start\|array_diff_uassoc\|array_diff_ukey\|array_filter\|array_intersect_uassoc\|array_intersect_ukey\|array_map\|array_reduce\|array_udiff_assoc\|array_udiff_uassoc\|array_udiff\|array_uintersect_assoc\|array_uintersect_uassoc\|array_uintersect\|array_walk_recursive\|array_walk\|assert_options\|uasort\|uksort\|usort\|preg_replace_callback\|spl_autoload_register\|iterator_apply\|call_user_func\|call_user_func_array\|register_shutdown_function\|register_tick_function\|set_error_handler\|set_exception_handler\|session_set_save_handler\|sqlite_create_aggregate\|sqlite_create_function\|phpinfo\|posix_mkfifo\|posix_getlogin\|posix_ttyname\|getenv\|get_current_user\|proc_get_status\|get_cfg_var\|disk_free_space\|disk_total_space\|diskfreespace\|getcwd\|getlastmo\|getmygid\|getmyinode\|getmypid\|getmyuid\|extract\|parse_str\|putenv\|ini_set\|mail\|header\|proc_nice\|proc_terminate\|proc_close\|pfsockopen\|fsockopen\|apache_child_terminate\|posix_kill\|posix_mkfifo\|posix_setpgid\|posix_setsid\|posix_setuid\|fopen\|tmpfile\|bzopen\|gzopen\|chgrp\|chmod\|chown\|copy\|lchgrp\|lchown\|link\|mkdir\|move_uploaded_file\|rename\|rmdir\|symlink\|tempnam\|touch\|unlink\|imagepng\|imagewbmp\|image2wbmp\|imagejpeg\|imagexbm\|imagegif\|imagegd\|imagegd2\|iptcembed\|ftp_get\|ftp_nb_get\|file_exists\|file\|fileatime\|filectime\|filegroup\|fileinode\|filemtime\|fileowner\|fileperms\|filesize\|filetype\|glob\|is_dir\|is_executable\|is_file\|is_link\|is_readable\|is_uploaded_file\|is_writable\|is_writeable\|linkinfo\|lstat\|parse_ini_file\|pathinfo\|readfile\|readlink\|realpath\|stat\|gzfile\|readgzfile\|getimagesize\|imagecreatefromgif\|imagecreatefromjpeg\|imagecreatefrompng\|imagecreatefromwbmp\|imagecreatefromxbm\|imagecreatefromxpm\|ftp_put\|ftp_nb_put\|exif_read_data\|read_exif_data\|exif_thumbnail\|exif_imagetype\|hash_file\|hash_hmac_file\|hash_update_file\|md5_file\|sha1_file\|highlight_file\|show_source\|php_strip_whitespace\|get_meta_tags\|curl_exec\|curl_multi_exec\|scandir\|chdir\|chroot\|closedir\|dir\|getcwd\|opendir\|readdir\|rewinddir' /var/www/ 2>/dev/null
 
 
 echo 
@@ -202,9 +217,8 @@ grep -r 'BEGIN OPENSSH PRIVATE KEY\|BEGIN RSA PRIVATE KEY' / 2>/dev/null
 
 
 echo 
-echo "Grabbing Hashes Inside Files (Comming Soon)"
+echo "Grabbing Hashes Inside Files(Comming soon)"
 echo 
-
 
 
 start_year=$(stat /etc/passwd |grep Modify |awk '{print $2}' | awk -F"-" '{print $1}')
